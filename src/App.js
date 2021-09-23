d26f0b38cddffd4f2b26e08fb9723050313e9900
Kin Yuunan <yuunan.kin@gmail.com>
2021-09-23 10:56:53 +0900

    define theme & access it globally

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { Fragment } from "react/cjs/react.production.min";
+import { ThemeProvider } from "styled-components";
 import { Header } from "./components/header";
 import { Container } from "./components/styles/container.styled";
 
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <Container>
-        <h1>hello </h1>
-      </Container>
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Header />
+        <Container>
+          <h1>hello </h1>
+        </Container>
+      </Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import { StyledHeader } from './styles/header.styled'
 
 export const Header = () => {
     return (
-        <StyledHeader bg={'#ebfbff'}>
-           <h1>Hubble</h1> 
+        <StyledHeader>
+            <h1>Hubble</h1>
         </StyledHeader>
     )
 }
